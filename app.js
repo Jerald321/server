d408f64aa9dd846f2d6b7632925dfcd8e3fbaaa7
Jerald321 <rjerald6803@gmail.com>
2024-12-29 17:47:20 +0530

    start-1

--- app.js
+++ app.js
@@ -1,69 +1,133 @@
-const express = require("express")
+// const express = require("express")
+// const app = express();
+// const http = require('http')
+// const   {Server} = require('socket.io')
+// const server =http.createServer(app) 
+// const cors = require('cors')
+// const io =new Server(server)
+// const cookieparser = require("cookie-parser")
+// const bodyParser = require('body-parser')
+
+
+
+// const corsOptions = {
+//   origin: 'http://localhost:5173', // Allow requests from this origin
+//   methods: ['GET', 'POST','PUT','DELETE'],
+// };
+// // app.use(cors(corsOptions))
+
+
+
+// io.on("connection", (socket) => {
+//     console.log("A user connected");
+  
+//     socket.on("disconnect", () => {
+//       console.log("A user disconnected");
+//     });
+//   });
+
+// app.use((req, res, next) => {
+//   req.io = io;
+//   next();
+// });
+
+
+// app.use(express.json())
+
+// const restaurantRoute =require('./route/restaurantRoute')
+
+// const allowedOrigins = [   "https://classy-basbousa-faedb0.netlify.app/"]
+
+
+// app.use(
+//     cors({
+//       origin: function (origin, callback) {
+//         if (!origin || allowedOrigins.includes(origin)) {
+//           callback(null, true);
+//         } else {
+//           callback(new Error('Not allowed by CORS'));
+//         }
+//       },
+//       credentials: true, // Ensure cookies are sent
+//       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//       allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+//     })
+//   );
+  
+
+
+ 
+//   app.use(bodyParser.urlencoded({ extended: true }));
+//   app.use(cookieparser());
+
+// app.use("/api", restaurantRoute )
+
+
+
+
+
+// module.exports = app
+
+
+
+const express = require("express");
 const app = express();
-const http = require('http')
-const   {Server} = require('socket.io')
-const server =http.createServer(app) 
-const cors = require('cors')
-const io =new Server(server)
-const cookieparser = require("cookie-parser")
-const bodyParser = require('body-parser')
+const http = require('http');
+const { Server } = require('socket.io');
+const server = http.createServer(app);
+const cors = require('cors');
+const cookieparser = require("cookie-parser");
+const bodyParser = require('body-parser');
 
+const allowedOrigins = ["https://classy-basbousa-faedb0.netlify.app", "http://localhost:5173"];
 
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.error(`Blocked by CORS: ${origin}`);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'Authorization',
+    ],
+  })
+);
 
-const corsOptions = {
-  origin: 'http://localhost:5173', // Allow requests from this origin
-  methods: ['GET', 'POST','PUT','DELETE'],
-};
-// app.use(cors(corsOptions))
-
-
+const io = new Server(server, {
+  cors: {
+    origin: allowedOrigins,
+    methods: ['GET', 'POST'],
+  },
+});
 
 io.on("connection", (socket) => {
-    console.log("A user connected");
-  
-    socket.on("disconnect", () => {
-      console.log("A user disconnected");
-    });
+  console.log("A user connected");
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
   });
+});
 
 app.use((req, res, next) => {
   req.io = io;
   next();
 });
 
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieparser());
 
-app.use(express.json())
+const restaurantRoute = require('./route/restaurantRoute');
+app.use("/api", restaurantRoute);
 
-const restaurantRoute =require('./route/restaurantRoute')
-
-const allowedOrigins = [   "https://classy-basbousa-faedb0.netlify.app/"]
-
-
-app.use(
-    cors({
-      origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
-        } else {
-          callback(new Error('Not allowed by CORS'));
-        }
-      },
-      credentials: true, // Ensure cookies are sent
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
-    })
-  );
-  
-
-
- 
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(cookieparser());
-
-app.use("/api", restaurantRoute )
-
-
-
-
-
-module.exports = app
+module.exports = app;
